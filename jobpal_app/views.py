5a68a2fb140e536aadb29865f6b03435afbbc7ee
mussabtafal <mussab_tafal@hotmail.com>
2022-02-26 21:43:16 +0200

    Joblist Backend

--- jobpal_app/views.py
+++ jobpal_app/views.py
@@ -1,3 +1,6 @@
+from asyncio.windows_events import NULL
+from multiprocessing import context
+from unicodedata import category
 from django.shortcuts import render,redirect
 from .models import User, Company,Job
 from django.contrib import messages
@@ -8,12 +11,6 @@ def landing(request):
 def about_us(request):
     return render (request, 'about_us.html')
 
-def jobs(request):
-    return render (request, 'job_list.html')
-
-def job_detail(request):
-    return render (request, 'job_detail.html')
-
 def companies(request):
     return render (request, 'company_list.html')
 
@@ -22,6 +19,65 @@ def company_profile(request):
 
 def add_job(request):
     return render (request, 'add_job.html')
+
+def jobs(request):
+    all_jobs = Job.objects.all()
+    all_companies = Company.objects.all()
+    context = {
+        "jobs": all_jobs,
+        "companies": all_companies
+    }
+    return render (request, 'job_list.html', context)
+
+def job_detail(request, job_id):
+    this_job = Job.objects.get(id = job_id)
+    all_jobs = Job.objects.all()
+    context = {
+        "job": this_job,
+        "jobs": all_jobs
+    } 
+    return render (request, 'job_detail.html', context)
+
+def f_category(request):
+    if 'f_category' in request.POST:
+        if request.POST['f_category'] == "":
+            return redirect("/jobs")
+        if 'f_company' in request.session:
+            del request.session['f_company']
+        request.session['f_category'] = request.POST['f_category']
+        return redirect("/jobs/filteredJobs")
+    else:
+        if request.POST['f_company'] == "":
+                return redirect("/jobs")
+        if 'f_category' in request.session:
+            del request.session['f_category']
+        request.session['f_company'] = request.POST['f_company']
+        return redirect("/jobs/filteredJobs")
+
+def render_category(request):
+    if 'f_category' in request.session:
+        cat_filter = Job.objects.filter(category = request.session['f_category'])
+        all_jobs = Job.objects.all()
+        all_companies = Company.objects.all()
+        context = {
+            "filteredJobs": cat_filter,
+            "jobs": all_jobs,
+            "companies": all_companies
+        }
+        return render(request, "job_list_filter.html", context)
+    else:
+        this_company = Company.objects.get(company_name = request.session['f_company'])
+        com_filter = Job.objects.filter(company_job = this_company)
+        print(com_filter)
+        print("*"*200)
+        all_jobs = Job.objects.all()
+        all_companies = Company.objects.all()
+        context = {
+            "filteredJobs": com_filter,
+            "jobs": all_jobs,
+            "companies": all_companies
+        } 
+        return render(request, "job_list_filter.html", context)
 
 def new (request):
     errors = Job.objects.basic_validator(request.POST)
@@ -35,4 +91,19 @@ def new (request):
         desc=request.POST['desc'],category=request.POST['category'],company_job=this_company)
         messages.success(request, "Job successfully added")
         return redirect('/add_job')
+
+def job_apply(request, job_id):
+    if not request.session['user_id']:
+        return redirect('/')
+    else:
+        this_job = Job.objects.get(id = job_id)
+        this_user = User.objects.get(id = request.session['user_id'])
+        all_appliers = this_job.user_job.all()
+        if this_user in all_appliers:
+            messages.success(request,"Already Applied")
+            return redirect('/')
+        else:
+            this_job.user_job.add(this_user)
+            messages.success(request,"Successfully Applied")
+            return redirect('/')
 
